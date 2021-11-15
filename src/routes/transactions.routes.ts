621f1ab54d3b7efabd91511c82f17171a6c3aca5
masprog2022 <masprog2022@gmail.com>
2021-11-15 14:06:27 +0100

    add import file att: error no test

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,15 +1,21 @@
 import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
+import multer from 'multer';
 
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
+
+import uploadConfig from '../config/upload';
 
 const transactionsRouter = Router();
 
+const upload = multer(uploadConfig);
+
 transactionsRouter.get('/', async (request, response) => {
   // TODO
+
   const transactionRepository = getCustomRepository(TransactionsRepository);
   const balance = await transactionRepository.getBalance();
 
@@ -34,11 +40,48 @@ transactionsRouter.post('/', async (request, response) => {
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const deleteTransaction = new DeleteTransactionService();
+
+  await deleteTransaction.execute({ id });
+
+  return response.status(204).send();
 });
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+transactionsRouter.post(
+  '/import',
+  upload.single('file'),
+  async (request, response) => {
+    const importTransaction = new ImportTransactionsService();
+
+    const transactions = await importTransaction.execute(request.file.path);
+
+    const createTransaction = new CreateTransactionService();
+
+    for (const transaction of transactions) {
+      await createTransaction.execute({
+        title: transaction.title,
+        type: transaction.type,
+        value: transaction.value,
+        category: transaction.category,
+      });
+    }
+
+    // MAP FUNCTION WAS NOT PROPERLY WORKING IN SOME SITUATIONS.
+    // transactions.map(async transaction => {
+    // const createTransaction = new CreateTransactionService();
+
+    // await createTransaction.execute({
+    // title: transaction.title,
+    // value: transaction.value,
+    // type: transaction.type,
+    // category: transaction.category,
+    // });
+    // });
+
+    return response.json(transactions);
+  },
+);
 
 export default transactionsRouter;
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,8 @@
 import { getCustomRepository, getRepository } from 'typeorm';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import AppError from '../errors/AppError';
 // import AppError from '../errors/AppError';
 
 interface RequestDTO {
@@ -18,11 +20,35 @@ class CreateTransactionService {
     category,
   }: RequestDTO): Promise<Transaction> {
     const transactionsRepository = getCustomRepository(TransactionsRepository);
+    // verificar se a categoria ja existe
+    const categoryRepository = getRepository(Category);
+
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('You do not have enough balance');
+    }
+
+    let transactioCategory = await categoryRepository.findOne({
+      where: {
+        title: category,
+      },
+    });
+
+    if (!transactioCategory) {
+      // Nao existe ? cria ela
+      transactioCategory = categoryRepository.create({
+        title: category,
+      });
+
+      await categoryRepository.save(transactioCategory);
+    }
 
     const transaction = transactionsRepository.create({
       title,
       value,
       type,
+      category: transactioCategory,
     });
 
     await transactionsRepository.save(transaction);
